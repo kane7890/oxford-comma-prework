95307f00c86329c9b5098bb080b1c676b5a680c4
Kane Koo <kanekoo@gmail.com>
2019-03-23 19:35:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-  length
+  length = array,length
+  if (length == 1)
+    arraystr = array[length-1]
+  elsif (length == 2)
+    arraystr = (array[0], array[1]).join ",")
+  else
+    arraystr = ""
+    for i in 0..(length-2)
+      phrase = array[i] <<","
+      arraystr << phrase
+    end
+    arraystr << "and "
+    arraystr << array[length-1]
+  end
+    
 end
